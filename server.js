9b5b1895979096b2d0dc6d2f2e39dc54ec97ed4a
jannat28-349 <jucse2.349@gmail.com>
2023-06-20 20:38:51 +0600

    User created

--- server.js
+++ server.js
@@ -5,10 +5,17 @@ const bodyParser = require("body-parser");
 
 app.use(bodyParser.json());
 
+const users = [];
+
 app.get("/", (req, res) => {
   res.json({ msg: "app successful" });
 });
 
+app.post("/users", (req, res) => {
+  const user = req.body;
+  users.push(user);
+  res.status(201).json(user);
+});
 
 app.listen(port, () => {
   console.log(`App is listening at port ${port}`);
