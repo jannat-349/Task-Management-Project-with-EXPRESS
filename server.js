09a73c1caa65ea67772a88e4df663ffbfbe63eb2
jannat28-349 <jucse2.349@gmail.com>
2023-06-20 21:17:01 +0600

    Get specific user

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ const bodyParser = require("body-parser");
 app.use(bodyParser.json());
 
 const users = [];
+let id = 0;
 
 app.get("/", (req, res) => {
   res.json({ msg: "app successful" });
@@ -13,12 +14,22 @@ app.get("/", (req, res) => {
 
 app.post("/users", (req, res) => {
   const user = req.body;
+  user.id = ++id;
   users.push(user);
   res.status(201).json(user);
 });
 
 app.get('/users', (req, res) => {
     res.status(200).json(users);
+})
+
+app.get('/users/:id', (req, res) => {
+    const id = req.params.id;
+    const user = users.find((u) => u.id==id)
+    if(user) {
+        res.status(200).json(user);
+    }
+    res.status(400).send(`User not found!!`)
 })
 
 app.listen(port, () => {
