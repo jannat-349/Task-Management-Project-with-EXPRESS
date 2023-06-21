621f9af863abbd2c447111c9e73ad6e26349f9eb
jannat28-349 <jucse2.349@gmail.com>
2023-06-21 10:58:02 +0600

    update User

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const bodyParser = require("body-parser");
 
 app.use(bodyParser.json());
 
-const users = [];
+let users = [];
 let id = 0;
 
 app.get("/", (req, res) => {
@@ -19,18 +19,31 @@ app.post("/users", (req, res) => {
   res.status(201).json(user);
 });
 
-app.get('/users', (req, res) => {
-    res.status(200).json(users);
-})
+app.get("/users", (req, res) => {
+  res.status(200).json(users);
+});
 
-app.get('/users/:id', (req, res) => {
-    const id = req.params.id;
-    const user = users.find((u) => u.id==id)
-    if(user) {
-        res.status(200).json(user);
-    }
-    res.status(400).send(`User not found!!`)
-})
+app.get("/users/:id", (req, res) => {
+  const id = req.params.id;
+  const user = users.find((u) => u.id == id);
+  if (user) {
+    res.status(200).json(user);
+  }
+  res.status(400).send(`User not found!!`);
+});
+
+app.put("/users/:id", (req, res) => {
+  const id = req.params.id;
+  const body = req.body;
+  const user = users.find((u) => u.id == id);
+  if (user) {
+    user.name = body.name;
+    user.age = body.age;
+    res.status(200).json(user);
+  } else {
+    res.status(404).json({ message: "User not found!!!" });
+  }
+});
 
 app.listen(port, () => {
   console.log(`App is listening at port ${port}`);
