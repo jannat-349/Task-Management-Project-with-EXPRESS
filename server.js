85d7048414b644ecacadb2dae10189093fb44ae7
jannat28-349 <jucse2.349@gmail.com>
2023-06-21 19:30:07 +0600

    mongodb connected

--- server.js
+++ server.js
@@ -1,9 +1,15 @@
 require("dotenv").config();
+const mongoose = require("mongoose");
 const express = require("express");
 const app = express();
 const bodyParser = require("body-parser");
 
 app.use(bodyParser.json());
+
+const uri = process.env.MONGODB_URI;
+mongoose.connect(uri, { useNewUrlParser: true }).then(() => {
+  console.log("Database connected...");
+});
 
 let users = [];
 let id = 0;
