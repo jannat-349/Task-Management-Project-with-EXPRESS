28731f27dc6d41b7c83764034a340d2d0110dd80
jannat28-349 <jucse2.349@gmail.com>
2023-06-25 22:48:11 +0600

    user created in mongoDB

--- server.js
+++ server.js
@@ -18,8 +18,18 @@ async function connectToMongoDB() {
 }
 connectToMongoDB();
 
-let users = [];
-let id = 0;
+
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  age: {
+    type: Number,
+  },
+});
+
+const User = mongoose.model("User", userSchema);
 
 app.get("/", (req, res) => {
   res.json({ msg: "app successful" });
@@ -27,51 +37,19 @@ app.get("/", (req, res) => {
 
 app.post("/users", (req, res) => {
   const user = req.body;
-  user.id = ++id;
-  users.push(user);
-  res.status(201).json(user);
+  const newUser = new User({
+    name: user.name,
+    age: user.age,
+  });
+  newUser
+    .save()
+    .then((savedUser) => {
+      res.status(201).json(savedUser);
+    })
+    .catch((error) => {
+      res.status(404).send("User not created!!");
+    });
 });
-
-app.get("/users", (req, res) => {
-  res.status(200).json(users);
-});
-
-app.get("/users/:id", (req, res) => {
-  const user = getUserByID(req);
-  if (user) {
-    res.status(200).json(user);
-  }
-  res.status(400).send(`User not found!!`);
-});
-
-app.put("/users/:id", (req, res) => {
-  const body = req.body;
-  const user = getUserByID(req);
-  if (user) {
-    user.name = body.name;
-    user.age = body.age;
-    res.status(200).json(user);
-  } else {
-    res.status(404).json({ message: "User not found!!!" });
-  }
-});
-
-app.delete("/users/:id", (req, res) => {
-  const id = req.params.id;
-  const userIndex = users.findIndex((u) => u.id == id);
-  if (userIndex) {
-    users.splice(userIndex, 1);
-    res.json(users);
-  } else {
-    res.status(404).json({ message: "User not found!!!" });
-  }
-});
-
-function getUserByID(req) {
-  const id = req.params.id;
-  const user = users.find((u) => u.id == id);
-  return user;
-}
 
 const port = process.env.PORT;
 app.listen(port, () => {
