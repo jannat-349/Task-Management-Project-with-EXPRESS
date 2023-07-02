1ec2e2ac600d2de37559a7fac76bb0cc56b86330
jannat28-349 <jucse2.349@gmail.com>
2023-07-02 13:28:13 +0600

    access token and refresh token added

--- server.js
+++ server.js
@@ -61,25 +61,75 @@ app.post("/users", async (req, res) => {
   }
 });
 
+async function handleEmailLogin(email, res, password) {
+  const user = await User.findOne({ email: email });
+  if (user) {
+    const isValidPassword = bcrypt.compare(password, user.password);
+    if (isValidPassword) {
+      const accesssToken = jwt.sign(
+        { email: user.email, id: user._id },
+        process.env.JWT_SECRET,
+        { expiresIn: "2m" }
+      );
+      const refreshToken = jwt.sign({ id: user._id }, process.env.JWT_SECRET, {
+        expiresIn: "3m",
+      });
+      const userObj = user.toJSON();
+      userObj["accessToken"] = accesssToken;
+      userObj["refreshToken"] = refreshToken;
+      res.status(200).json(userObj);
+    } else {
+      res.status(401).json({ message: `Wrong Password!!` });
+    }
+  } else {
+    res.status(404).json({ message: `User Not Found` });
+  }
+}
+
+async function handleRefreshLogin(refreshToken, res) {
+  if (!refreshToken) {
+    res.status(404).json({ message: `No refresh token defined` });
+  } else {
+    jwt.verify(refreshToken, process.env.JWT_SECRET, async (err, payload) => {
+      if (err) {
+        res.status(401).json({ message: `Unauthorized` });
+      } else {
+        const id = payload.id;
+        const user = await User.findById(id);
+        if (user) {
+          const accesssToken = jwt.sign(
+            { email: user.email, id: user._id },
+            process.env.JWT_SECRET,
+            { expiresIn: "2m" }
+          );
+          const refreshToken = jwt.sign(
+            { id: user._id },
+            process.env.JWT_SECRET,
+            { expiresIn: "3m" }
+          );
+          const userObj = user.toJSON();
+          userObj["accessToken"] = accesssToken;
+          userObj["refreshToken"] = refreshToken;
+          res.status(200).json(userObj);
+        } else {
+          res.status(404).json({ message: `User Not Found` });
+        }
+      }
+    });
+  }
+}
+
 app.post("/users/login", async (req, res) => {
   try {
-    const { email, password } = req.body;
-    const user = await User.findOne({ email: email });
-    if (user) {
-      const isValidPassword = await bcrypt.compare(password, user.password);
-      if (isValidPassword) {
-        const token = jwt.sign(
-          { email: user.email, id: user._id },
-          process.env.JWT_SECRET, { expiresIn: '10m'}
-        );
-        const userObj = user.toJSON();
-        userObj["accessToken"] = token;
-        res.status(200).json(userObj);
-      } else {
-        res.status(401).json({ message: `Wrong Password!!` });
-      }
+    const { email, password, type, refreshToken } = req.body;
+    if (!type) {
+      res.status(404).json({ message: `type is not defined` });
     } else {
-      res.status(404).json({ message: `User Not Found` });
+      if (type == "email") {
+        await handleEmailLogin(email, res, password);
+      } else {
+        await handleRefreshLogin(refreshToken, res);
+      }
     }
   } catch (error) {
     res.status(500).send(`Something Went wrong`);
@@ -154,7 +204,7 @@ app.delete("/profile", authenticateToken, async (req, res) => {
   }
 });
 
-app.get("/users", async (req, res) => {
+app.get("/users", authenticateToken, async (req, res) => {
   try {
     const users = await User.find({});
     res.status(200).json(users);
