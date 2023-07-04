45fb2b912e6316216ad13a03425ee4e0bf8eb6d9
jannat28-349 <jucse2.349@gmail.com>
2023-07-04 21:20:33 +0600

    login user route modularized

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const router = express.Router();
 const bcrypt = require("bcrypt");
+const User = require("../../models/User");
+const jwt = require("jsonwebtoken");
 
 router.post("/", async (req, res) => {
   try {
@@ -26,5 +28,70 @@ router.post("/", async (req, res) => {
     res.status(500).send(`Something Went wrong`);
   }
 });
+
+router.post("/login", async (req, res) => {
+  try {
+    const { email, password, type, refreshToken } = req.body;
+    if (!type) {
+      res.status(404).json({ message: `type is not defined` });
+    } else {
+      if (type == "email") {
+        await handleEmailLogin(email, res, password);
+      } else {
+        await handleRefreshLogin(refreshToken, res);
+      }
+    }
+  } catch (error) {
+    res.status(500).send(`Something Went wrong`);
+  }
+});
+
+function getUserTokens(user, res) {
+  const accesssToken = jwt.sign(
+    { email: user.email, id: user._id },
+    process.env.JWT_SECRET,
+    { expiresIn: "2m" }
+  );
+  const refreshToken = jwt.sign({ id: user._id }, process.env.JWT_SECRET, {
+    expiresIn: "3m",
+  });
+  const userObj = user.toJSON();
+  userObj["accessToken"] = accesssToken;
+  userObj["refreshToken"] = refreshToken;
+  res.status(200).json(userObj);
+}
+async function handleEmailLogin(email, res, password) {
+  const user = await User.findOne({ email: email });
+  if (user) {
+    const isValidPassword = await bcrypt.compare(password, user.password);
+    if (isValidPassword) {
+      getUserTokens(user, res);
+    } else {
+      res.status(401).json({ message: `Wrong Password!!` });
+    }
+  } else {
+    res.status(404).json({ message: `User Not Found` });
+  }
+}
+
+async function handleRefreshLogin(refreshToken, res) {
+  if (!refreshToken) {
+    res.status(404).json({ message: `No refresh token defined` });
+  } else {
+    jwt.verify(refreshToken, process.env.JWT_SECRET, async (err, payload) => {
+      if (err) {
+        res.status(401).json({ message: `Unauthorized` });
+      } else {
+        const id = payload.id;
+        const user = await User.findById(id);
+        if (user) {
+          getUserTokens(user, res);
+        } else {
+          res.status(404).json({ message: `User Not Found` });
+        }
+      }
+    });
+  }
+}
 
 module.exports = router;
